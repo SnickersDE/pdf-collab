a41a07e184bd59694e992553c2d2e3eea2a12619
SnickersDE <florian_97@live.de>
2025-11-02 18:27:07 +0100

    Restore configuration comments and placeholders

--- config.js
+++ config.js
@@ -6,3 +6,4 @@
 const SUPABASE_URL = "https://luketbkguzdfdlogrbub.supabase.co"; // <-- TODO
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1a2V0YmtndXpkZmRsb2dyYnViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIwNzg1MDcsImV4cCI6MjA3NzY1NDUwN30.Ia54K_JdQVH2BJvh-ef6e7rsFQZ_VE63ch68tcnwuIk"; // <-- TODO
 const BUCKET_NAME = "pdf-collab"; // <-- passe an, falls du anderen Bucket-Namen verwendest
+
